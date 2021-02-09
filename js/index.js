9ccb0e83f8b66b2aaff01226ce6547ef9bffc70f
Chudi Ibida <chudiibida3@gmail.com>
2021-02-09 15:27:00 -0500

    chudi ibida

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+
+// #1
+let heading = document.querySelector('.logo-heading');
+heading.addEventListener('dblclick',(event) => {
+    event.target.style.color = 'navy';
+})
+
+// #2
+let headerColor = document.querySelector('.intro > p');
+headerColor.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'gold';
+    event.target.style.borderRadius = '5px';
+    event.target.style.textAlign = 'center';
+})
+
+// #3
+let headerColorBack = document.querySelector('.intro > p');
+headerColorBack.addEventListener('mouseleave', (event) => {
+    event.target.style.backgroundColor = 'white';
+    event.target.style.borderRadius = '0px';
+    event.target.style.textAlign = 'left';
+})
+
+// #4
+
